03f27df47a730316aecf4b9563dc8dadb6fa3ba6
ishasrivas <srivastavaisha714@gmail.com>
2023-07-27 16:53:15 +0530

    first commit restraunt

---
+++ reactprojectapp/src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Restraunt from "./Components/Basics/Restraunt";
+//import UseState from "./Components/Hooks/UseState";
+//import UseReducer from "./Components/Hooks/UseReducer";
+
+const App = () =>{
+return(
+<Restraunt />
+);
+
+
+};
+
+export default App;
+
+
+
+
+
+
+// const App = () =>
+// {
+//   return (
+//     <>
+//     <MyName />
+//     <h1>Hello World</h1>
+//     <p>Welcome</p>
+    
+//     </>
+//   );
+// };
+
+// const MyName = () =>
+// {
+//   return <h1>My name is ISHA</h1>;
+// };
---
+++ reactprojectapp/src/Components/Basics/MenuApi.js
@@ -0,0 +1,121 @@
+const Menu = [
+{
+    id: 1,
+    image: "images/maggi.jpg",
+    name: "Maggi",
+    category: "breakfast",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 2,
+    image: "images/allupakoida.jpg",
+    name: "aloo pakoda",
+    category: "breakfast",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj",
+},
+{
+    id: 3,
+    image: "images/paubhaji.jpg",
+    name: "pav bhaji",
+    category: "breakfast",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 4,
+    image: "images/samosa.jpg",
+    name: "samosa",
+    category: "breakfast",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 5,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "breakfast",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 6,
+    image: "images/chola.jpg",
+    name: "chola",
+    category: "lunch",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 7,
+    image: "images/momo.jpg",
+    name: "momo",
+    category: "lunch",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 8,
+    image: "images/pizza.jpg",
+    name: "pizza",
+    category: "lunch",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 9,
+    image: "images/puri.jpg",
+    name: "puri",
+    category: "lunch",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 4,
+    image: "images/rajmarice.jpg",
+    name: "rajmarice",
+    category: "lunch",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 11,
+    image: "images/sweet.jpg",
+    name: "sweet",
+    category: "dinner",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 12,
+    image: "images/nonvegthali.jpg",
+    name: "nonvegthali",
+    category: "dinner",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 13,
+    image: "images/rotiwithmeat.jpg",
+    name: "rotiwithmeat",
+    category: "dinner",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+{
+    id: 14,
+    image: "images/vegthali.jpg",
+    name: "vegthali",
+    category: "dinner",
+    price: "30Rs",
+    description: "I love Maggi. Jhgghgsd jhgsdhg jhbsjhf hhg hbhd hjh hjf hjgdfbjhfb hhfh hfgdjhjf jfh jhdj hdh hdjfh jhfjh jheiuie uwyygjbkj llk skdk kjweie klmk jfjkshf sjdioo.",
+},
+
+
+
+
+
+];
+
+export default Menu;
